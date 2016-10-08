bca02e2b97bc077358a1d7c260c4352e22aa5089
macramole <leandrogarber@gmail.com>
2016-10-07 21:29:40 -0300

    fuzzy

--- ciencia/tp2/tarea_1_fuzzy.r
+++ ciencia/tp2/tarea_1_fuzzy.r
@@ -1,6 +1,25 @@
 library(cluster)
 library(MASS)
 library(fpc)
+library(doMC)
 
-df = read.csv("data_clean.csv")
+registerDoMC(3)
+
+df = read.csv("../tp1/data_tp1.csv",row.names = 1)
+
+summary(df)
+rownames(df)
+nrow(df)
+
+df.dist = dist(df, method = "euclidean")
+
+#barrer k, memb.exp
+df.fuzzes = foreach(varK = 2:10) %dopar% {
+  fanny( df.dist, k = varK, diss = T,  memb.exp = 1.5, keep.diss = F, keep.data = F)
+}
+df.fuzz = fanny( df.dist, k = 4, diss = T,  memb.exp = 1.5, keep.diss = F, keep.data = F)
+
+summary(df.fuzz)
+head(df.fuzz$membership)
+#df.fuzzy =
 
