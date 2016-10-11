4b6c93c8fe213b8eec8215af759809c5b446e76a
macramole <leandrogarber@gmail.com>
2016-10-11 00:22:31 -0300

    fuzzy

--- ciencia/tp2/tarea_1_fuzzy.r
+++ ciencia/tp2/tarea_1_fuzzy.r
@@ -13,13 +13,46 @@ nrow(df)
 
 df.dist = dist(df, method = "euclidean")
 
+varK = 3
+varMemb.exp = 1.7
+
+cat("dataset","k","memb.exp","silhuette_avg.width","coeff","time", file = "fuzzy_results.csv", sep = ",", append = F, fill = T)
+
 #barrer k, memb.exp
-df.fuzzes = foreach(varK = 2:10) %dopar% {
-  fanny( df.dist, k = varK, diss = T,  memb.exp = 1.5, keep.diss = F, keep.data = F)
+foreach(varK = 2:11) %dopar% {
+  for ( varMemb.exp in seq(1.2,1.7,0.1) ) {
+    t0 = Sys.time()
+    df.fuzz = fanny( df.dist, k = varK, diss = T,  memb.exp = varMemb.exp, keep.diss = F, keep.data = F)
+    t1 = Sys.time()
+    # cat("Tiempo: ", as.numeric(t1 - t0))
+    cat("tp1", varK, varMemb.exp, df.fuzz$silinfo$avg.width, df.fuzz$coeff[2], as.numeric(t1 - t0, units = "secs"),
+        file = "fuzzy_results.csv", sep = ",", append = T, fill = T)
+  }
 }
-df.fuzz = fanny( df.dist, k = 4, diss = T,  memb.exp = 1.5, keep.diss = F, keep.data = F)
+# df.fuzz = fanny( df.dist, k = 4, diss = T,  memb.exp = 1.5, keep.diss = F, keep.data = F)
 
 summary(df.fuzz)
 head(df.fuzz$membership)
 #df.fuzzy =
+
+f=read.csv("/home/macramole/Desktop/dm/2do_cuatri/ciencia/tp2/rProject/ciencia/tp2/fuzzy_results.csv")
+#head(f)
+table(f$k) 
+
+a = c()
+for ( i in min(f$k):max(f$k) ) {
+  kFiltered = f[ f$k == i , ]
+  maxSilhuetteIndex = which.max(kFiltered[,c("silhuette_avg.width")])
+  
+  a = rbind(a, kFiltered[maxSilhuetteIndex,] )
+}
+a
+
+which
+
+library("plotly")
+library(dplyr)
+
+plot_ly(a, x = k, y= silhuette_avg.width) %>%
+  add_trace(x = k, y = coeff)
 
