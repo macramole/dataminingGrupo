89a0d895abed6409a0b5a6fecc782cf755b486b8
macramole <leandrogarber@gmail.com>
2016-10-11 02:01:00 -0300

    fuzzy

--- ciencia/tp2/tarea_1_fuzzy.r
+++ ciencia/tp2/tarea_1_fuzzy.r
@@ -30,22 +30,78 @@ foreach(varK = 2:11) %dopar% {
   }
 }
 
-f=read.csv("fuzzy_results.csv")
-#head(f)
-table(f$k) 
+#Analizo resultados
 
-a = c()
-for ( i in min(f$k):max(f$k) ) {
-  kFiltered = f[ f$k == i , ]
+fuzzyResults =read.csv("fuzzy_results.csv")
+#head(f)
+table(fuzzyResults$k) 
+
+fuzzyResults.max = c()
+for ( i in min(fuzzyResults$k):max(fuzzyResults$k) ) {
+  kFiltered = fuzzyResults[ fuzzyResults$k == i , ]
   maxSilhuetteIndex = which.max(kFiltered[,c("silhuette_avg.width")])
   
-  a = rbind(a, kFiltered[maxSilhuetteIndex,] )
+  fuzzyResults.max = rbind(fuzzyResults.max, kFiltered[maxSilhuetteIndex,] )
 }
 
 
 library("plotly")
 library(dplyr)
 
-plot_ly(a, x = k, y= silhuette_avg.width) %>%
-  add_trace(x = k, y = coeff)
+plot_ly(fuzzyResults.max, x = k, y= silhuette_avg.width, name="silhuette") %>%
+  add_trace(x = k, y = coeff, name="coeff")
 
+plot_ly(fuzzyResults[fuzzyResults$k == 2,], x = memb.exp, y = silhuette_avg.width, name = "k = 2")
+add_trace(fuzzyResults[fuzzyResults$k == 3,], x = memb.exp, y = silhuette_avg.width, name = "k = 3")
+add_trace(fuzzyResults[fuzzyResults$k == 4,], x = memb.exp, y = silhuette_avg.width, name = "k = 4")
+
+plot_ly(fuzzyResults[fuzzyResults$k == 2,], x = memb.exp, y = coeff, name = "k = 2")
+add_trace(fuzzyResults[fuzzyResults$k == 3,], x = memb.exp, y = coeff, name = "k = 3")
+add_trace(fuzzyResults[fuzzyResults$k == 4,], x = memb.exp, y = coeff, name = "k = 4")
+
+
+
+fuzzyResults.max
+
+df.fuzzy.2 = fanny( df.dist, k = 2, diss = T,  memb.exp = 1.2, keep.diss = F, keep.data = F)
+df.fuzzy.3 = fanny( df.dist, k = 3, diss = T,  memb.exp = 1.2, keep.diss = F, keep.data = F)
+df.fuzzy.4 = fanny( df.dist, k = 4, diss = T,  memb.exp = 1.2, keep.diss = F, keep.data = F)
+
+df.fuzzy.2.negSil = df.fuzzy.2$silinfo$widths[ df.fuzzy.2$silinfo$widths[,3] < 0, ]
+cbind ( df.fuzzy.2$membership[ rownames(df.fuzzy.2.negSil), ], df.fuzzy.2.negSil[,3] )
+df.fuzzy.2.highSil = df.fuzzy.2$silinfo$widths[ df.fuzzy.2$silinfo$widths[,3] > 0.4, ]
+cbind ( df.fuzzy.2$membership[ rownames(df.fuzzy.2.highSil), ], df.fuzzy.2.highSil[,3] )
+
+df.fuzzy.3.negSil = df.fuzzy.3$silinfo$widths[ df.fuzzy.3$silinfo$widths[,3] < 0, ]
+cbind ( df.fuzzy.3$membership[ rownames(df.fuzzy.3.negSil), ], df.fuzzy.3.negSil[,3] )
+
+pdf("asd.pdf")
+plot(silhouette(df.fuzzy.2))
+plot(silhouette(df.fuzzy.3))
+plot(silhouette(df.fuzzy.4))
+dev.off()
+
+
+#intento mejorar el coeff 
+varK = 2
+
+foreach(varMemb.exp = seq(1,1.2,0.02)) %dopar% {
+  t0 = Sys.time()
+  df.fuzz = fanny( df.dist, k = varK, diss = T,  memb.exp = varMemb.exp, keep.diss = F, keep.data = F)
+  t1 = Sys.time()
+  # cat("Tiempo: ", as.numeric(t1 - t0))
+  cat("tp1", varK, varMemb.exp, df.fuzz$silinfo$avg.width, df.fuzz$coeff[2], as.numeric(t1 - t0, units = "secs"),
+      file = "fuzzy_results.csv", sep = ",", append = T, fill = T)
+}
+
+
+fuzzyResults %>%
+  filter( k == 2, coeff > 0 ) %>%
+  arrange(memb.exp) %>%
+  plot_ly(x = memb.exp, y = coeff  )
+
+
+#reflexion
+#
+# creo que la posta es elegir un K con el menor "ruido" (membership > 0.6), en este caso
+# parecería ser el 2
