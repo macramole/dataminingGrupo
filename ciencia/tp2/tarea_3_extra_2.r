1723f6c5010cadaf0f2ef6e27ebe1e7c4831741d
macramole <leandrogarber@gmail.com>
2016-10-17 19:54:41 -0300

    extra

--- ciencia/tp2/tarea_3_extra_2.r
+++ ciencia/tp2/tarea_3_extra_2.r
@@ -1,12 +1,29 @@
 library(arules)
 
 df = read.csv("data_clean.csv",row.names = 1)
-df.xy <- df[,c(1,2)]
+
+df.xy <- df[,c(2,3)]
 df <- df[-c(1,2)]
 
 summary(df$MC_z)
 hist(df$MC_z)
-df.MC_z.discret = discretize(df$MC_z,method = "interval",categories = 3)
+df.MC_z.discret = discretize(df$MC_z,method = "interval",categories = 4)
 
-qplot(df.xy$x,df.xy$y, color = df.MC_z.discret ) + labs(title="Corrimiento al rojo sobre X e Y",x="X",y="Y", colour= "MC_z estandarizado")
-qplot(df.xy$x,df.xy$y, color = df$MC_z ) + labs(title="Corrimiento al rojo sobre X e Y",x="X",y="Y", colour= "MC_z estandarizado")
+df.xy = scale(df.xy)
+
+col = c("#000000", "#848484", "#dbdbdb")
+col = c("#f93e3e", "#f98f3e", "#ffff00")
+col = c("#ff2b2b", "#ff872b", "#2b78ff", "#522bff")
+
+qplot(df.xy$x,df.xy$y, color = df.MC_z.discret ) + scale_colour_manual(values=col) + scale_size( range = c(0.1) ) + theme(panel.background = element_rect(fill = 'white')) + labs(title="Corrimiento al rojo sobre X e Y",x="X",y="Y", colour= "MC_z")
+qplot(df.xy$x,df.xy$y, alpha = df$MC_z ) +  labs(title="Corrimiento al rojo sobre X e Y",x="X",y="Y", colour= "MC_z")
+
+str(base)
+base = read.csv("data_clean.csv",row.names = 1)
+base.xy = base[,c(1,2)]
+base = base[,-c(1,2)]
+
+K1<- kmeans(x = base, 3 , iter.max = 50)
+col <- c("#766272","#4BDBC0","#F5AF3B")
+qplot(base.xy$x,base.xy$y, color = as.factor(K1$cluster) )  + scale_colour_manual(values=col) + theme(panel.background = element_rect(fill = 'white')) + labs(title="Corrimiento al rojo sobre X e Y",x="X",y="Y", colour= "MC_z")
+
